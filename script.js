67bea9628db83db63378923ce9791256b6357cd9
malikkhawer <khawer0310aliameer@gmail.com>
2024-08-14 01:23:58 -0700

    javascript classes upload

---
+++ script.js
@@ -0,0 +1,191 @@
+
+
+
+
+// let studentName = prompt("Enter your Name");
+// document.write("studentName:" + studentName + "<br>");
+
+// let eng = parseInt(prompt("Enter your English Marks"));
+// let urdu = parseInt(prompt("Enter your urdu Marks"));
+// let bio = parseInt(prompt("Enter your biology Marks"));
+ 
+// let obtainedMarks = eng + urdu + bio;
+// let percentage =(obtainedMarks / 300) * 100;
+
+// document.write("English Marks:" + eng + "<br>");
+// document.write("urdu Marks:" + urdu + "<br>");
+// document.write("biology Marks:" + bio + "<br>");
+
+// document.write("percentage:" + percentage + "<br>");
+
+// if(percentage <= 100 && percentage >= 80){
+//     document.write("Grade:A+");
+// }
+// else if(percentage <= 79 && percentage >= 70){
+//     document.write("Grade:A");
+// } 
+// else if(percentage <= 69 && percentage >= 60){
+//     document.write("Grade:B");
+// }
+// else if(percentage <= 59 && percentage >= 50){
+//     document.write("Grade:C");
+// }
+// else{ 
+//     document.write("Result is Failed");
+// }
+
+
+
+
+
+
+// let studentName = prompt("Enter your name");
+// document.write("StudentName:" + studentName + "<br>");
+
+// let eng = parseInt(prompt("Enter your english marks"));
+// let urdu = parseInt(prompt("Enter your urdu marks"));
+// let math = parseInt(prompt("Enter your mathematics marks"));
+
+
+// let obtainedMarks = (eng + urdu + math);
+// let percentage = (obtainedMarks / 300) * 100;
+
+// document.write("English Marks:" + eng + "<br>");
+// document.write("Urdu Marks:" + urdu + "<br>");
+// document.write("Mathematics Marks:" + math + "<br>");
+
+// document.write("Percentage:" + percentage + "<br>");
+
+// if(percentage <= 100 && percentage >= 80){
+//     document.write("Grade:A+");
+// }
+
+// else if(percentage <= 79 && percentage >= 70){
+//     document.write("Grade:A");
+// }
+
+// else if(percentage <= 69 && percentage >= 60){
+//     document.write("Grade:B");
+// }
+
+// else if(percentage <= 59 && percentage >= 50){
+//     document.write("Grade:C");
+// }
+// else{ 
+//     document.write("Result Fail");
+
+// }
+
+// let yname = prompt("Enter your name");
+// if(yname == "admin"){
+//     document.write(
+//         "you are eligible"
+//     ); 
+// }
+// else{
+//     alert("Try again");
+// }
+
+
+
+// let studentName =  prompt("Enter your name");
+// document.write("StudentName:" + studentName + "<br>");
+
+// let sub1  = parseInt(prompt("Enter your sub1 marks"));
+// document.write("sub1 marks:" + sub1 + "<br>");
+// let sub2 = parseInt(prompt("Enter your sub2 marks"));
+// document.write("sub2 marks:" + sub2 + "<br>");
+// let sub3 = parseInt(prompt("Enter your sub3 marks"));
+// document.write("sub3 marks:" + sub3 + "<br>");
+
+// let obtainedMarks = sub1 + sub2 + sub3;
+
+// let percentage = (obtainedMarks / 300) * 100;
+// document.write("percentage:" + percentage + "<br>");
+
+// if(percentage <= 100 && percentage >= 80){
+//     document.write("Grade:A+");
+// }
+// else if(percentage <= 79 && percentage >= 70){
+//     document.write("Grade:A");
+// }
+// else if(percentage <= 69 && percentage >= 60){
+//     document.write("Grade:B");
+// }
+// else if(percentage <= 59 && percentage >= 50){
+//     document.write("Grade:C");
+// }
+// else{
+//     document.write("Result Fail");
+// }
+
+
+
+
+
+
+// for(let i = 0; i <= 10; i++){
+//     console.log(i)
+// }
+
+// let names = ["khawer","sultan","babar","waseem"]
+
+// for(let i = 0; i <= 4;){
+//     document.write(names [4] + "<br>");
+// }
+
+
+let number = parseInt(prompt('Enter an integer:'));
+
+for(let i = 1; i <= 10; i++){
+    const  result = i * number ;
+    console.log(`${number} * ${i} = ${result}`);
+}
+
+
+
+// // program to generate a multiplication table
+
+// // take input from the user
+// const number = parseInt(prompt('Enter an integer: '));
+
+// //creating a multiplication table
+// for(let i = 1; i <= 10; i++) {
+
+//     // multiply i with number
+//     const result = i * number;
+
+//     // display the result
+//     console.log(`${number} * ${i} = ${result}`);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
